68e4b6b8df26bac4fbe74c8b763d08fa003e53ce
vladnen97 <darkdescent1991@gmail.com>
2023-03-21 17:11:27 +0300

    import file upload and use it

--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
-const errorHandling = require('./middleware/ErrorHandlingMiddleware')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
 
 //обработка ошибок, последний Middleware
-app.use(errorHandling)
+app.use(errorHandler)
 
 
 const start = async () => {
