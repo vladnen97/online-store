66585d20c25bdfa09ed6c3ca659ed5e6f36fa397
vladnen97 <darkdescent1991@gmail.com>
2023-03-22 19:41:17 +0300

    add check role middleware

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,10 +1,12 @@
 const Router = require('express')
 const brandController = require('../controllers/brandController')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
+
 
 const router = new Router()
 
 
-router.post('/', brandController.create)
+router.post('/', checkRoleMiddleware('ADMIN'), brandController.create)
 router.get('/', brandController.getAll)
 
 module.exports = router
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const deviceController = require('../controllers/deviceController')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
 const router = new Router()
 
 
-router.post('/', deviceController.create)
+router.post('/', checkRoleMiddleware('ADMIN'), deviceController.create)
 router.get('/', deviceController.getAll)
 router.get('/:id', deviceController.getDeviceById)
 
